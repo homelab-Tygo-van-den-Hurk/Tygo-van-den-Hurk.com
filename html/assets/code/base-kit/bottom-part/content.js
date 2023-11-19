7fa3568f3032d1b576a71e311451f40e9d508aec
St-h123 <hazels.05.remount@icloud.com>
2023-11-19 12:20:08 +0000

    almost done fixing bugs and getting it ready

--- html/assets/code/base-kit/bottom-part/content.js
+++ html/assets/code/base-kit/bottom-part/content.js
@@ -9,28 +9,28 @@ document.write( // Copy froom contents.html until we find a way to fetch this in
         <button type="button" class="shadow rounded" id="bottom-button">Let&rsquo;s talk!</button>
         <div id="bottom-list-of-socials"><!-- display 5 at a time max is done via css-->
             <a href="https://facebook.com/profile.php?id=100011384536668" id="bottom-list-of-socials-facebook-icon">
-                <img alt="facebook icon" src="/assets/images/icons/socials/facebook-white.svg" class="dark-mode">
-                <img alt="facebook icon" src="/assets/images/icons/socials/facebook-black.svg" class="light-mode">
+                <img alt="facebook icon" src="/assets/images/icons/socials/facebook-white.svg"> <!-- class="dark-mode"> -->
+                <!-- <img alt="facebook icon" src="/assets/images/icons/socials/facebook-black.svg" class="light-mode"> -->
             </a>
             <a href="https://www.instagram.com/tygo.van.den.hurk/" id="bottom-list-of-socials-instagram-icon">
-                <img alt="instagram icon" src="/assets/images/icons/socials/instagram-white.svg" class="dark-mode">
-                <img alt="instagram icon" src="/assets/images/icons/socials/instagram-black.svg" class="light-mode">
+                <img alt="instagram icon" src="/assets/images/icons/socials/instagram-white.svg"> <!-- class="dark-mode"> -->
+                <!-- <img alt="instagram icon" src="/assets/images/icons/socials/instagram-black.svg" class="light-mode"> -->
             </a>
             <a href="https://t.snapchat.com/fWkBlmdb" id="bottom-list-of-socials-snapchat-icon">
-                <img alt="snapchat icon" src="/assets/images/icons/socials/snapchat-white.svg" class="dark-mode">
-                <img alt="snapchat icon" src="/assets/images/icons/socials/snapchat-black.svg" class="light-mode">
+                <img alt="snapchat icon" src="/assets/images/icons/socials/snapchat-white.svg"> <!-- class="dark-mode"> -->
+                <!-- <img alt="snapchat icon" src="/assets/images/icons/socials/snapchat-black.svg" class="light-mode"> -->
             </a>
             <a href="https://www.linkedin.com/in/tygo-van-den-hurk" id="bottom-list-of-socials-linkedin-icon">
-                <img alt="linkedin icon" src="/assets/images/icons/socials/linkedin-white.svg" class="dark-mode">
-                <img alt="linkedin icon" src="/assets/images/icons/socials/linkedin-black.svg" class="light-mode">
+                <img alt="linkedin icon" src="/assets/images/icons/socials/linkedin-white.svg"> <!-- class="dark-mode"> -->
+                <!-- <img alt="linkedin icon" src="/assets/images/icons/socials/linkedin-black.svg" class="light-mode"> -->
             </a>
             <a href="/error-pages/404.html" id="bottom-list-of-socials-whatsapp-icon">
-                <img alt="whatsapp icon" src="/assets/images/icons/socials/whatsapp-white.svg" class="dark-mode">
-                <img alt="whatsapp icon" src="/assets/images/icons/socials/whatsapp-black.svg" class="light-mode">
+                <img alt="whatsapp icon" src="/assets/images/icons/socials/whatsapp-white.svg"> <!-- class="dark-mode"> -->
+                <!-- <img alt="whatsapp icon" src="/assets/images/icons/socials/whatsapp-black.svg" class="light-mode"> -->
             </a>
             <a href="/error-pages/404.html" id="bottom-list-of-socials-email-icon">
-                <img alt="email icon" src="/assets/images/icons/socials/email-white.svg" class="dark-mode">
-                <img alt="email icon" src="/assets/images/icons/socials/email-black.svg" class="light-mode">
+                <img alt="email icon" src="/assets/images/icons/socials/email-white.svg"> <!-- class="dark-mode"> -->
+                <!-- <img alt="email icon" src="/assets/images/icons/socials/email-black.svg" class="light-mode"> -->
             </a>
         </div>
     </div>
