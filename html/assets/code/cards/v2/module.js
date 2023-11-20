bbdf3ca0e01190465875c137547251a5fe7122a9
St-h123 <hazels.05.remount@icloud.com>
2023-11-20 12:06:29 +0000

    changed some files to .SHTML so that SSI can work later

--- html/assets/code/cards/v2/module.js
+++ html/assets/code/cards/v2/module.js
@@ -28,11 +28,6 @@ export function printCards(cards) {
                         "card.title was not of type String. This is not allowed."
                     );
                 }
-                if (card.title == null) {
-                    throw new Error(functionName + ".pre violated, " +
-                        "card.title was null. This is not allowed."
-                    );
-                }
                 if (card.title === "") {
                     throw new Error(functionName + ".pre violated, " +
                         "card.title is an empty String. This is not allowed."
@@ -46,27 +41,17 @@ export function printCards(cards) {
                         "card.text was not of type String. This is not allowed."
                     );
                 }
-                if (card.text == null) {
-                    throw new Error(functionName + ".pre violated, " +
-                        "card.text was null. This is not allowed."
-                    );
-                }
                 if (card.text === "") {
                     throw new Error(functionName + ".pre violated, " +
                         "card.text is an empty String. This is not allowed."
                     );
                 }
             }
-            
+
             /* Checking the Card-imagePath */ {
                 if (!(typeof card.imagePath === "String")) {
                     throw new Error(functionName + ".pre violated, " +
                         "card.imagePath was not of type String. This is not allowed."
-                    );
-                }
-                if (card.imagePath == null) {
-                    throw new Error(functionName + ".pre violated, " +
-                        "card.imagePath was null. This is not allowed."
                     );
                 }
                 if (card.imagePath === "") {
@@ -75,16 +60,11 @@ export function printCards(cards) {
                     );
                 }
             }
-            
+
             /* Checking the Card-imageAlt */ {
                 if (!(typeof card.imageAlt === "String")) {
                     throw new Error(functionName + ".pre violated, " +
                         "card.imageAlt was not of type String. This is not allowed."
-                    );
-                }
-                if (card.imageAlt == null) {
-                    throw new Error(functionName + ".pre violated, " +
-                        "card.imageAlt was null. This is not allowed."
                     );
                 }
                 if (card.imageAlt === "") {
@@ -100,7 +80,12 @@ export function printCards(cards) {
         for (const card of cards) {
             document.write( // Copy from contents.html until we find a way to fetch this instead.
             `
-            <p> A card was printed here </p>
+                <div class="box">
+                    <p> A card was printed here </p>
+                    <p> ${card.title} </p>
+                    <p> ${card.text} </p>
+                    <img src="${card.title}" alt="${card.imageAlt}">
+                </div>
             `);
         }
     }
@@ -126,21 +111,62 @@ export function printCards(cards) {
  *   \result.imagePath === imagePath
  *   \result.imageAlt === imageAlt &&
  * }
+ * @throws Error when Pre-Condition is violated.
  */
 export function Card(title, text, imagePath, imageAlt) {
+
     /* Checking Pre-Conditions */ {
+        /* Checking the Card-Title */ {
+            if (!(typeof title === "String")) {
+                throw new Error(functionName + ".pre violated, " +
+                    "title was not of type String. This is not allowed."
+                );
+            }
+            if (title === "") {
+                throw new Error(functionName + ".pre violated, " +
+                    "title is an empty String. This is not allowed."
+                );
+            }
+        }
 
-        if (!(typeof title === "String")) {throw new Error;}
-        if (title === "") {throw new Error;}
+        /* Checking the Card-text */ {
+            if (!(typeof text === "String")) {
+                throw new Error(functionName + ".pre violated, " +
+                    "text was not of type String. This is not allowed."
+                );
+            }
+            if (text === "") {
+                throw new Error(functionName + ".pre violated, " +
+                    "text is an empty String. This is not allowed."
+                );
+            }
+        }
 
-        if (!(typeof text === "String")) {throw new Error;}
-        if (text === "String") {throw new Error;}
-        
-        if (!(typeof imagePath === "String")) {throw new Error;}
-        if (typeof imagePath === "") {throw new Error;}
-        
-        if (!(typeof imageAlt === "String")) {throw new Error;}
-        if (imageAlt === "") {throw new Error;}
+        /* Checking the Card-imagePath */ {
+            if (!(typeof imagePath === "String")) {
+                throw new Error(functionName + ".pre violated, " +
+                    "imagePath was not of type String. This is not allowed."
+                );
+            }
+            if (imagePath === "") {
+                throw new Error(functionName + ".pre violated, " +
+                    "imagePath is an empty String. This is not allowed."
+                );
+            }
+        }
+
+        /* Checking the Card-imageAlt */ {
+            if (!(typeof imageAlt === "String")) {
+                throw new Error(functionName + ".pre violated, " +
+                    "imageAlt was not of type String. This is not allowed."
+                );
+            }
+            if (imageAlt === "") {
+                throw new Error(functionName + ".pre violated, " +
+                    "imageAlt is an empty String. This is not allowed."
+                );
+            }
+        }
     }
 
     /* Creating a Card*/ {
@@ -149,7 +175,7 @@ export function Card(title, text, imagePath, imageAlt) {
             text: text || `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                 Officia sapiente, illo nobis deserunt ipsa perspiciatis qui ea, suscipit 
                 ipsum tempore nihil illum velit earum? Quae ducimus totam velit.`,
-            imagePath: imagePath || "/assets/images/paterns/Inn.svg",
+            imagePath: imagePath || "/assets/images/paterns/Aare.svg",
             imageAlt: imageAlt || "no discription was provided"
         };
     }
